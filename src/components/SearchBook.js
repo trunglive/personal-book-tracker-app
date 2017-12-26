1908d7b8c3d2554f243f836d901f054d57e65702
Trung <trunglive@gmail.com>
2017-12-26 00:14:41 -0500

    removed the draft in SearchBook.js

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -114,53 +114,6 @@ class SearchBook extends Component {
                   );
                 })}
               </ol>
-
-              {/* <ol className='books-grid'>
-                {books.map(book => {
-                  return (
-                    <li key={book.id}>
-                      <div className='book'>
-                        <div className='book-top'>
-                          <a href={book.canonicalVolumeLink} target='_blank'>
-                            <div
-                              className='book-cover'
-                              style={{
-                                width: 128,
-                                height: 193,
-                                backgroundImage: `url(${
-                                  book.imageLinks.thumbnail
-                                })`
-                              }}
-                            />
-                          </a>
-                          <div className='book-shelf-changer'>
-                            <select
-                              value={book.shelf ? book.shelf : 'default'}
-                              onChange={event =>
-                                onMoveBook(book, event.target.value)
-                              }
-                            >
-                              <option value='default' disabled>
-                                Move to...
-                              </option>
-                              <option value='currentlyReading'>
-                                Currently Reading
-                              </option>
-                              <option value='wantToRead'>Want to Read</option>
-                              <option value='read'>Read</option>
-                              <option value='none'>None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className='book-title'>{book.title}</div>
-                        <div className='book-authors'>
-                          {book.authors ? book.authors.join(', ') : 'No Author'}
-                        </div>
-                      </div>
-                    </li>
-                  );
-                })}
-              </ol> */}
             </div>
           </div>
         )}
